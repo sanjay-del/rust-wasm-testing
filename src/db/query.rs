f9f38fbe9c42af8da51d9d747796429a39296826
Sanjay <sanjaypantha.np@gmail.com>
2024-10-23 12:02:10 +0545

    update query for persistent storage

--- src/db/query.rs
+++ src/db/query.rs
@@ -1,5 +1,6 @@
 use super::index::Index;
 use super::storage::Storage;
+use std::io;
 
 pub struct Query {
     pub storage: Storage,
@@ -31,11 +32,22 @@ impl Query {
     pub fn search(&self, key: &str) -> Option<&Vec<String>> {
         self.index.get(key)
     }
+
+    pub fn save(&self, filename: &str) -> io::Result<()> {
+        self.storage.save_to_file(filename)
+    }
+
+    pub fn load(&mut self, filename: &str) -> io::Result<()> {
+        let load_storage = Storage::load_from_file(filename)?;
+        self.storage = load_storage;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
 
     #[test]
     fn test_set_get() {
@@ -63,5 +75,22 @@ mod tests {
         let result = query.search("key1").unwrap();
         assert!(result.contains(&"value1".to_string()));
         assert!(result.contains(&"value2".to_string()));
+    }
+
+    #[test]
+    fn test_save_load_query() {
+        let mut query = Query::new();
+        query.set("key1".to_string(), "value1".to_string());
+        query.set("key2".to_string(), "value2".to_string());
+
+        query.save("test_query_db.json").unwrap();
+
+        let mut loaded_query = Query::new();
+        loaded_query.load("test_query_db.json").unwrap();
+
+        assert_eq!(loaded_query.get("key1"), Some(&"value1".to_string()));
+        assert_eq!(loaded_query.get("key2"), Some(&"value2".to_string()));
+
+        fs::remove_file("test_query_db.json").unwrap();
     }
 }
