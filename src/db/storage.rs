add3c7d7d227960016816d80e510c5fa375112ad
Sanjay <sanjaypantha.np@gmail.com>
2024-10-23 11:34:42 +0545

    persistent storage

--- src/db/storage.rs
+++ src/db/storage.rs
@@ -1,5 +1,9 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, Read, Write};
 
+#[derive(Deserialize, Serialize)]
 pub struct Storage {
     pub store: HashMap<String, String>,
 }
@@ -19,10 +23,26 @@ impl Storage {
     pub fn delete(&mut self, key: &str) -> Option<String> {
         self.store.remove(key)
     }
+    pub fn save_to_file(&self, filename: &str) -> io::Result<()> {
+        let mut file = File::create(filename)?;
+        let serialized = serde_json::to_string(&self.store)?;
+        writeln!(file, "{}", serialized)?;
+        Ok(())
+    }
+
+    pub fn load_from_file(filename: &str) -> io::Result<Self> {
+        let mut file = File::open(filename)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        let store: HashMap<String, String> = serde_json::from_str(&contents)?;
+        Ok(Storage { store })
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
@@ -39,5 +59,20 @@ mod tests {
         storage.set("key1".to_string(), "value2".to_string());
         storage.delete("key1");
         assert_eq!(storage.get("key1"), None);
+    }
+
+    #[test]
+    fn test_save_load() {
+        let mut storage = Storage::new();
+        storage.set("key1".to_string(), "value1".to_string());
+        storage.set("key2".to_string(), "value2".to_string());
+
+        storage.save_to_file("test_db.json").unwrap();
+
+        let load_storage = Storage::load_from_file("test_db.json").unwrap();
+        assert_eq!(load_storage.get("key1"), Some(&"value1".to_string()));
+        assert_eq!(load_storage.get("key2"), Some(&"value2".to_string()));
+
+        fs::remove_file("test_db.json").unwrap();
     }
 }
